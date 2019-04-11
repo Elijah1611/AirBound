f16a5fbf94e511ea6738e95e076451a0c5e6c103
Paul <lijahthedev@gmail.com>
2019-04-11 13:32:31 -0500

    Added Google Maps Embed

--- js/app.js
+++
@@ -1,8 +0,0 @@
-var map;
-function initMap() {
-    var locationEast = {lat: 39.290, lng: -76.612};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 7,
-      center: locationEast,
-      gestureHandling: 'greedy'
-    })};
